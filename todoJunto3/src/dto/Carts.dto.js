07fb352cf5c52db3d4c210d65b880fa36fe26c63
aceved0coder <112910193+aceved0coder@users.noreply.github.com>
2023-09-12 23:45:20 -0300

    Delete todoJunto3 directory

--- todoJunto3/src/dto/Carts.dto.js
+++
@@ -1,9 +0,0 @@
-class CartDTO {
-    static getCartInputFrom = (cart) => {
-        return {
-            products: cart.products || []
-        };
-    };
-}
-
-module.exports = CartDTO;
--- todoJunto3/src/middleware/logger.middleware.js
+++
@@ -1,7 +0,0 @@
-const { logger } = require("../config/logger");
-
-exports.addLogger = (req, res, next) => {
-    req.logger = logger
-    req.logger.info(`${req.method} en ${req.url} - ${new Date().toLocaleString()}`)
-    next()
-}
